52568d12ebe2ff21635bdba145467b716a51c1d9
Kioshi Okamoto Rojas <kjor29@gmail.com>
2021-06-03 22:11:29 -0500

    :dart: Improve: inicializar repositorio, implementar swagger y variables de entorno

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Documentacion de proyecto')
+    .setDescription('Proyecto similar a workana / chambeala')
+    .setVersion('0.0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
+
+  await app.listen(process.env.PORT || 3000);
+}
+bootstrap();
